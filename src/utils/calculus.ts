37243d84daf462c6faa40c9c6b8e130d62cce393
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-19 10:34:37 +0200

    Add function to evaluate expression

--- src/utils/calculus.ts
+++ src/utils/calculus.ts
@@ -1,6 +1,16 @@
+const isNumeric = (str: string): boolean => /^\d+\.?\d*$/.test(str);
 
-const calculus = (query: string): Number => {
-  return 0;
+const isValidOperator = (str: string): boolean => ['+', '-', '*', '/', '(', ')'].includes(str)
+
+const isValidExpr = (str: string): boolean => /^[0-9+\-*/()\s]*$/.test(str)
+
+const calculate = (query: string): Number => {
+  if ( isValidExpr(query) ){
+    return Number(Number(eval(query) as number).toFixed(2));
+  }
+  throw "Invalid operation found in query";
 }
 
-export default calculus;
+calculate('2 * (23/(3*3)) - 23 / (2*3)');
+
+export default calculate;
