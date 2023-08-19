20cd5e4005d6fe9745f7bd6c063c11a010521010
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-19 14:23:03 +0200

    Update Calculus Entity

--- src/app/entity/Calculus.ts
+++ src/app/entity/Calculus.ts
@@ -1,29 +1,23 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm"
 
 @Entity()
 export class Calculus {
 
-  @PrimaryGeneratedColumn()
-  id: number
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-  @Column({
-    length: 25
-  })
-  ip: string
+  @Column({ length: 25, type: 'varchar' })
+  ip: string;
 
-  @Column({
-    length: 625
-  })
-  query: string
+  @Column({ length: 625 })
+  query: string;
 
   @Column("float")
-  result: number
+  result: number;
 
   @Column()
-  timeTaken: number
+  timeTaken: number;
 
-  @Column({
-    type: "timestamptz", default: () => "CURRENT_TIMESTAMP" 
-  })
-  createdAt: Date
+  @CreateDateColumn()
+  createdAt: Date;
 }
