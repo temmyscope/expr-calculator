da0f7d2c84582ccef87be56adac6c79894365a99
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-19 11:19:20 +0200

    Modify return type on calculate utility function

--- src/utils/calculus.ts
+++ src/utils/calculus.ts
@@ -1,16 +1,10 @@
-const isNumeric = (str: string): boolean => /^\d+\.?\d*$/.test(str);
-
-const isValidOperator = (str: string): boolean => ['+', '-', '*', '/', '(', ')'].includes(str)
-
 const isValidExpr = (str: string): boolean => /^[0-9+\-*/()\s]*$/.test(str)
 
-const calculate = (query: string): Number => {
+const calculate = (query: string): number => {
   if ( isValidExpr(query) ){
     return Number(Number(eval(query) as number).toFixed(2));
   }
-  throw "Invalid operation found in query";
+  throw "Invalid operation/symbol/character found in query";
 }
-
-calculate('2 * (23/(3*3)) - 23 / (2*3)');
 
 export default calculate;
