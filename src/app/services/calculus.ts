53f1e110e8a482c5134511a02ac16c50588ec9a9
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-19 11:19:53 +0200

    Add CalculusService

--- src/app/services/calculus.ts
+++ src/app/services/calculus.ts
@@ -1,1 +1,21 @@
+import calculate from "../../utils/calculus";
+import { CalculusRepositoryInterface, QueryHistory } from "../types";
 
+class CalculusService {
+
+  constructor(protected calculusRepo: CalculusRepositoryInterface) {}
+  
+  async getUserHistory(ip: string): Promise<Array<QueryHistory>> {
+    const userHistory = await this.calculusRepo.getHistory(ip);
+    return userHistory;
+  }
+  
+  async calculate(query: string, ip: string) {
+    const startTime  = performance.now();
+    let result = calculate(query);
+    let timeTaken  = performance.now() - startTime;
+    await this.calculusRepo.save(ip, query, result, timeTaken);
+  }
+}
+
+export default CalculusService
