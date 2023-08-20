b6d8e552b903fa46a617e84d9dfb835f8389181d
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-20 03:42:11 +0200

    Update tests to test implementation not details

--- test/calculus.test.ts
+++ test/calculus.test.ts
@@ -1,27 +1,27 @@
 import request from 'supertest';
 import calculus from '../src/app';
-import { describe, it,  } from '@jest/globals';
+import { describe, expect, it,  } from '@jest/globals';
 
 describe('GET /api/calculus', () => {
 
-  it('responds with correct response when valid encoding is sent', (done) => {
+  it('responds with correct response when valid encoding is sent', () => {
     request(calculus)
       .get(`/api/calculus?query=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=`) //2 * (23/(3*3))- 23 * (2*3)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        error: false, result: -132.89
-      }, done);
+      .set('Accept', 'application/json').expect('Content-Type', /json/)
+      .expect(200).then(res => {
+        expect(res.body.data).toBe({ error: false, result: -132.89 });
+      });
   }, 10000);
 
-  it('responds with error message when invalid operation is used in expression', (done) => {
+  it('responds with error message when invalid operation is used in expression', async() => {
     request(calculus)
       .get(`/api/calculus?query=MiUgb2YgKDIzLygzKjMpKS0gMjMgKiAoMiozKQ==`) //2% of (23/(3*3))- 23 * (2*3)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(422, {
-        error: true, message: "Invalid operation/symbol/character found in expression"
-      }, done);
+      .set('Accept', 'application/json').expect('Content-Type', /json/)
+      .expect(422).then(res => {
+        expect(res.body.data).toBe({ 
+          error: true, message: "Invalid operation/symbol/character found in expression" 
+        });
+      });
   }, 10000);
 
 });
--- test/history.test.ts
+++ test/history.test.ts
@@ -7,31 +7,16 @@ import { QueryHistory } from '../src/app/types';
 describe('GET /api/calculus/history', () => {
 
   it('responds with last 5 operations done by user', async () => {
-    const res = await request(calculus)
-      .get(`/api/calculus/history`)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        error: false, data: []
-      });
-
-    expect((res.body.data as Array<QueryHistory>).length).toBeLessThanOrEqual(5)
-  });
-
-  it('responds with last correct attributes in response data body', (done) => {
     request(calculus)
       .get(`/api/calculus/history`)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        assert(
-          (response.body.data as Array<QueryHistory>)[0].query, '2 *%(23/(3*3))- 23 * (2*3)'
-        );
-        assert(
-          (response.body.data as Array<QueryHistory>)[0].result, '-132.89'
-        );
-      }, done);
-  }, 10000);
+      .expect(200).then(res => {
+        expect((res.body.data as Array<QueryHistory>).length).toBeLessThanOrEqual(5)
+        expect(res.body.data[0]).toHaveProperty('query');
+        expect(res.body.data[0]).toHaveProperty('result');
+      });
+
+  });
 
 });
