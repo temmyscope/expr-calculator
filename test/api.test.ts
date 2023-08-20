ff497b1bc63a27b5be584de3b96d07ce92a2ca91
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-08-20 13:07:32 +0200

    Fix errors in test

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,7 +2,7 @@ import { describe, it } from '@jest/globals';
 import request from 'supertest';
 import app from '../src/app';
 
-describe('GET /api', () => {
+describe('Test to check if Api is properly setup', () => {
   it('responds with a json message', (done) => {
     request(app)
       .get('/api')
@@ -11,6 +11,14 @@ describe('GET /api', () => {
       .expect(200, {
         message: 'B24 Calculus API - 👋🌎🌍🌏'
       }, done);
+  });
+
+  it('responds with `not found` status code', (done) => {
+    request(app)
+      .get('/api/random')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
   });
 });
 
--- test/calculus.test.ts
+++ test/calculus.test.ts
@@ -6,19 +6,19 @@ describe('GET /api/calculus', () => {
 
   it('responds with correct response when valid encoding is sent', () => {
     request(calculus)
-      .get(`/api/calculus?query=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=`) //2 * (23/(3*3))- 23 * (2*3)
+      .get(`/api/calculus?query=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=`)
       .set('Accept', 'application/json').expect('Content-Type', /json/)
       .expect(200).then(res => {
-        expect(res.body.data).toBe({ error: false, result: -132.89 });
+        expect(res.body).toStrictEqual({ error: false, result: -132.89 });
       });
   }, 10000);
 
   it('responds with error message when invalid operation is used in expression', async() => {
     request(calculus)
-      .get(`/api/calculus?query=MiUgb2YgKDIzLygzKjMpKS0gMjMgKiAoMiozKQ==`) //2% of (23/(3*3))- 23 * (2*3)
+      .get(`/api/calculus?query=MiUgb2YgKDIzLygzKjMpKS0gMjMgKiAoMiozKQ==`)
       .set('Accept', 'application/json').expect('Content-Type', /json/)
       .expect(422).then(res => {
-        expect(res.body.data).toBe({ 
+        expect(res.body).toStrictEqual({ 
           error: true, message: "Invalid operation/symbol/character found in expression" 
         });
       });
