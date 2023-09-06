d4250df38a4b3f00a169782cae02eddec66fa857
Elisha OYAWALE <temmyscope@Elishas-Air.fritz.box>
2023-09-06 17:33:35 +0200

    Clean out calculus api handler and utils

--- src/api/calculus.ts
+++ src/api/calculus.ts
@@ -1,28 +1,27 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { query, validationResult } from 'express-validator';
-import CalculusRepository from '../app/repo/calculus';
-import CalculusService from '../app/services/calculus';
-import { InvalidOperationException } from '../app/exception';
-import { AppDataSource } from '../config/db';
+import CalculusController from '../app/controller/calculus.controller';
+import { InvalidOperationException } from '../app/exception/invalid';
 
 const router = express.Router();
 
-const calcRepo = new CalculusRepository(AppDataSource);
-const calculusService = new CalculusService(calcRepo);
+const calcController = new CalculusController();
 
 router.get(
   '/',
   query('query').notEmpty().isBase64(),
   async (req: Request, res: Response, next: NextFunction) => {
+    
     const result = validationResult(req);
     if (!result.isEmpty()) {
       return res.status(422).json(
         { error: true, message: result.array() },
       );
     }
+
     try {
-      const result = await calculusService.calculate(
-        Buffer.from(req.query.query as string, 'base64').toString('utf8'), req.ip,
+      const result = await calcController.calculate(
+        req.query.query as string, req.ip,
       );
       return res.status(200).json({ error: false, result });
     } catch (e) {
@@ -33,12 +32,13 @@ router.get(
       }
       next(e);
     }
+
   },
 );
 
 router.get('/history', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const history = await calculusService.getUserHistory(req.ip, 5, 0);
+    const history = await calcController.getHistory(req.ip, 5, 0);
     return res.status(200).json({ data: history });
   } catch (e) {
     res.status(500);
--- src/utils/calculus.ts
+++ src/utils/calculus.ts
@@ -1,24 +1,4 @@
-import { InvalidOperationException } from '../app/exception/index';
+export const isValidExpr = (str: string): boolean => /^[0-9+\-*/()\s]*$/.test(str);
 
-const isValidExpr = (str: string): boolean => /^[0-9+\-*/()\s]*$/.test(str);
 
-/**
- * evaluate valid mathematical expression to 2 decimal places
- * @param {string} expr
- *
- * @throws InvalidOperationException
- * @returns
- */
-const calculate = (expr: string): number => {
-  if (isValidExpr(expr)) {
-    try {
-      let result = Function("return "+ expr)();
-      return Number(Number(result as number).toFixed(2));
-    } catch (error) {
-      throw new InvalidOperationException();
-    }
-  }
-  throw new InvalidOperationException();
-};
-
-export default calculate;
+export const decodeExpr = (expr: string): string => Buffer.from(expr, 'base64').toString('utf8');
